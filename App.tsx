b21a389a1e60fba35249ef4bb70e460a0a31078b
Caio Siqueira <csiqueirasilva@gmail.com>
2023-08-09 23:04:22 -0300

    movida reacao para useeffect, adicionado mock de loading

--- App.tsx
+++ App.tsx
@@ -5,61 +5,66 @@ import { StyleSheet } from 'react-native';
 import axios from 'axios';
 
 const App = () => {
-  //const [apiResponse, setApiResponse] = useState<string | null>(null);
   const [number, setNumber] = useState<string | null>('');
-  const [input, setInput ] = useState<string | null>('');
+  const [input, setInput] = useState<string | null>('');
   const [gif, setGif] = useState<string | null>(null);
   const [response, setResponse] = useState<string | null>('');
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef<TextInput>(null);
-  const [leitura, setLeitura] = useState(false);
 
   useEffect(() => {
-    // console.log("carregou");
-    // console.log('current', inputRef.current);
-    // console.log('input', input);
-    let timeOut : NodeJS.Timer|null = null;
-    if (inputRef.current !== null && input===''){
+    let timeOut: NodeJS.Timer | null = null;
+    if (inputRef.current !== null && input === '') {
       timeOut = setTimeout(() => {
-        //console.log('entrou');
         inputRef.current?.focus();
-      }, 0) 
+      }, 50)
     }
     return () => {
-      if(timeOut !== null){
+      if (timeOut !== null) {
         clearTimeout(timeOut);
       }
     }
-  }, [inputRef, input])
+  }, [inputRef.current, input])
 
+  useEffect(() => {
+    let timeOut: NodeJS.Timer | null = null;
+    if (loading) {
+      const func = async () => {
+        if (number === '1553785378') {
+          try {
+            const response = await axios.get('https://yesno.wtf/api');
+            const img = response.data.image;
+            const answer = response.data.answer;
 
-  
-
-
-
+            setGif(img);
+            setResponse(answer);
+          } catch (error) {
+            console.error('Error fetching API:', error);
+          }
+        } else {
+          setResponse("Senha inválida");
+          setGif('https://www.quickanddirtytips.com/wp-content/uploads/2022/04/wrong-or-wrongly%20(3).jpg');
+        }
+        // forçando um delay para não deixar passar seguidamente; também é uma simulação para ver como seria bloquear um carregamento
+        timeOut = setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      };
+      func();
+    }
+    return () => {
+      if (timeOut !== null) {
+        clearTimeout(timeOut);
+      }
+    }
+  }, [loading]);
 
   const handleCheckGif = async () => {
-
-    setNumber(input);
-    setInput('');
-    
-    if (input === '1553785378') {
-      try {
-        
-        const response = await axios.get('https://yesno.wtf/api');
-        const img = response.data.image;
-        const answer = response.data.answer;
-
-        setGif(img);
-        setResponse(answer);
-      } catch (error) {
-        console.error('Error fetching API:', error);
-      }
-    } else {
-      setResponse("Senha inválida");
-      setGif('https://www.quickanddirtytips.com/wp-content/uploads/2022/04/wrong-or-wrongly%20(3).jpg');
+    if (!loading) { // isso não deixa o usuário fazer outra requisição enquanto essa não termina
+      setLoading(true);
+      setNumber(input);
+      setInput('');
     }
-
-    
   };
 
   // useEffect(() => {
@@ -71,7 +76,7 @@ const App = () => {
   //       // Make an API call
   //       const response = await axios.get('https://yesno.wtf/api');
   //       const answer = response.data.answer;
-        
+
   //       // Update state to display API response on the screen
   //       setApiResponse(answer);
   //     } catch (error) {
@@ -80,37 +85,44 @@ const App = () => {
   //   });
   // }, []);
 
-
   return (
     <View style={styles.conteiner}>
-      <Text>Cartão Aqui</Text>
-      <TextInput
-      ref={inputRef}
-      style={styles.input}
-      onChangeText={setInput}
-      value={input || ''}
-      onSubmitEditing={handleCheckGif}
-      />
-      <Button title="Check Gif" onPress={handleCheckGif} />
-      {gif && (
-        <>
-          <Image
-            source={{ uri: gif }} 
-            style={{ marginTop: 20, width: 200, height: 200 }} 
-          />
-          <Text>{response}</Text>
-          <Text>Você leu o número: {number}</Text>
-        </>
-      )}
+      {
+        loading &&
+          <View style={{ display: 'flex', justifyContent: 'center', position: 'absolute', width: '100%', height: '100%', zIndex: 1000, backgroundColor: "rgba(0.5,0.5,0.5,0.25)" }}>
+            <Text style={{ alignSelf: 'center', padding: 20, backgroundColor: 'rgba(1,1,1,0.25)' }}>Loading...</Text>
+          </View> 
+      }
+      <>
+        <Text>Cartão Aqui</Text>
+        <TextInput
+          ref={inputRef}
+          style={styles.input}
+          onChangeText={setInput}
+          value={input || ''}
+          onSubmitEditing={handleCheckGif}
+        />
+        <Button title="Check Gif" onPress={handleCheckGif} />
+        {gif && !loading && (
+          <>
+            <Image
+              source={{ uri: gif }}
+              style={{ marginTop: 20, width: 200, height: 200 }}
+            />
+            <Text style={{ fontWeight: "bold", fontSize: 36 }}>{response}</Text>
+            <Text>Você leu o número: {number}</Text>
+          </>
+        )}
+      </>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  conteiner: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center' 
+  conteiner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   input: {
     height: 40,
